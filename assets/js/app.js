6b0e828accbbbb7762a30977e497bf8208ee81f9
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-03-19 13:58:17 +0000

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyA3ra4E7chQhRzERyKW2aFUTRULMb10vxM",
     authDomain: "blogdaffadev.firebaseapp.com",
     projectId: "blogdaffadev",
-    databaseURL: "https://blogdaffadev-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    databaseURL: "https://blogdaffadev-default-rtdb.asia-southeast1.firebasedatabase.app",
     storageBucket: "blogdaffadev.firebasestorage.app",
     messagingSenderId: "489703180131",
     appId: "1:489703180131:web:4383f5ef04c1d414ce3556",
